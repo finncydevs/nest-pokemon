eb224a09286b83cc7db362dacb4ec3dbd12e74af
finn <finnnxptrix@gmail.com>
2025-03-23 17:52:56 +0700

    try

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemons/pokemons.module';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
-console.log("this is in app module",process.env.MONGO_URL);
+console.log('this is in app module', process.env.MONGO_URL); // Debugging
+
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      process.env.MONGO_URL as string || '',
-      {},
-       
-    ),
+    MongooseModule.forRoot((process.env.MONGO_URL as string) || '', {}),
     PokemonModule,
   ],
 })
